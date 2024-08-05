1657ba69a8758d84a58c1cf2b2993a33ed602fd5
samarpanuprety <samarpanurna@gmail.com>
2024-08-05 09:23:22 +0545

    inihi

---
+++ src/assignment.java
@@ -0,0 +1,31 @@
+//import java.util.Scanner;
+//public class assignment {
+//        public static void main(String[] args) {
+//            Scanner takeInput = new Scanner(System.in);
+//
+//            System.out.println("Enter the number : ");
+//            int number = takeInput.nextInt();
+//
+//            for (int i = 0; i <= 100; i++) {
+//                System.out.println("Guess the number : ");
+//                int guess = takeInput.nextInt();
+//                if (guess < number) {
+//                    System.out.println("Cold");
+//                } else if (guess > number) {
+//                    System.out.println("Hot");
+//                } else {
+//                    System.out.println("The guess is correct.");
+//                    break;
+//                }
+//            }
+//        }
+//    }
+//
+//
+
+public class assignment{
+    public static void main(String[] args) {
+        int [] myarray = new int [3];
+        int [] myArray = {1,2,3};
+    }
+}
