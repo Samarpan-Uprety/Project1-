ed92c118b1082ab18396288c099c46695c58faae
Samarpan-Uprety <samarpanurna@gmail.com>
2024-08-07 21:39:13 +0545

    Main.java

--- Billing System/src/Main.java
+++ Billing System/src/Main.java
@@ -15,7 +15,6 @@ public class Main {
         itemNum = money.nextInt();
 
         ArrayList<Double> priceList = new ArrayList<>();
-        System.out.println("Enter the prices of the items you purchased.");
         System.out.println("Type (quit/Quit)if you want to exit early!");
         String wassup = money.nextLine();
         for (int i = 0; i < itemNum; i++) {
